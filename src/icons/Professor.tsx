33384a3f5c658ca0fb042285123ea54591019a66
Jiahao Li <jiahaoli0465@gmail.com>
2025-03-04 15:36:13 -0500

    fix html tags

--- src/icons/Professor.tsx
+++ src/icons/Professor.tsx
@@ -20,11 +20,11 @@ const ProfessorIcon: React.FC<IconProps> = ({
       viewBox="0 0 512 512"
       xmlSpace="preserve"
     >
-      <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+      <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
       <g
         id="SVGRepo_tracerCarrier"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       ></g>
       <g id="SVGRepo_iconCarrier">
         <path
