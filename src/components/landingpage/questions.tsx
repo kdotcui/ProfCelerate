64b4dbde41b4708e1405a40d774bdf92ccd9941e
Kevin Cui <106274250+kdotcui@users.noreply.github.com>
2025-03-14 11:26:48 -0400

    added option to opne multiple in accordion

--- src/components/landingpage/questions.tsx
+++ src/components/landingpage/questions.tsx
@@ -32,7 +32,7 @@ export const Questions: React.FC = () => {
       <h1 className="text-5xl md:text-6xl font-semibold mb-6 tracking-normal items-center text-center">
         Questions
       </h1>
-      <Accordion type="single" className="px-20" collapsible>
+      <Accordion type="multiple" className="px-20" collapsible>
         {faqData.map((item,index) => (
           <AccordionItem value={`item-${index}`} className="hover:bg-zinc-100 data-[state=open]:bg-zinc-100">
             <AccordionTrigger className="text-xl font-semibold">{item.question}</AccordionTrigger>
